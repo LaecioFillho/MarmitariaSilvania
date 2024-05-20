fe96c3b9a1b7aa52d8562a709a89cc93daaf36c0
Laecio Filho <141887207+LaecioFillho@users.noreply.github.com>
2024-05-20 14:39:48 +0000

    feat: config pre-deploy

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -2,16 +2,16 @@ import { HiShoppingCart } from "react-icons/hi";
 
 export default function Cart(){
 
-    const openCartItens = () => {
+    //const openCartItens = () => {
         
-    }
+    //}
 
     return(
         <>
             <footer className="fixed bottom-0 z-40 flex items-center justify-center w-full p-4 bg-green-700 text-white">
                 <button 
                     className="flex items-center gap-2"
-                    onClick={() => openCartItens()}>
+                    >
                         <HiShoppingCart className="h-5 w-5"/>
                         Ver Pedido
                 </button>
--- src/components/CartItens.tsx
+++ src/components/CartItens.tsx
@@ -1,9 +1,5 @@
 export default function CartItens(){
 
-    const openCart = () => {
-        
-    }
-
     return(
         <>
             <section 
